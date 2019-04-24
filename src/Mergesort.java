5f8cb9d2bb57af50aa6ea957389e6fff66b6cafb
lujin <532978773@qq.com>
2019-04-24 14:41:44 +0800

    Initial commit

---
+++ src/Mergesort.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Mergesort {
+    static void merge(int[] t, int left, int mid, int right){
+        if(left==right)
+            return;
+        int len=right-left;
+        int[] temp=new int[len];
+        int i=left;
+        int j=mid;
+        int index=0;
+        while ((i<mid)&&(j<right)){
+            temp[index++]=t[i]<t[j]?t[i++]:t[j++];
+        }
+        while(i<mid)
+            temp[index++]=t[i++];
+        while(j<right)
+            temp[index++]=t[j++];
+        for (int k = 0; k < len; k++) {
+            t[k+left]=temp[k];
+        }
+    }
+    public static void main(String[] args) {
+        int[] a={11,2,9,4,5,7,7,4,12};
+        int left=0,mid,right;
+        for (int i = 1; i < a.length; i*=2) {
+            left=0;
+                while(left+i<a.length){
+                    mid=left+i;
+                    right=mid+i<a.length-1?mid+i:a.length-1;
+                    merge(a,left,mid,right);
+                    left=right;
+                }
+        }
+        System.out.printf(Arrays.toString(a));
+        int[] b={11,2,9,4,5,7,7,4,12};
+
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,18 @@
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        String a ="a";
+        a+="b";
+        System.out.printf("a");
+        StringBuffer stringBuffer=new StringBuffer();
+//       new ObservableList<>()
+//        SortedList sortedList=new SortedList();
+
+    }
+}
